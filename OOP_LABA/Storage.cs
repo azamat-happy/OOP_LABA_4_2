70b01e4be0381be040e65e60772d82fbdad9e29b
Азамат Шарипов <azamatfootball1003@outlook.com>
2022-01-10 12:07:53 +0500

    Добавил хрнаилище Storage

--- OOP_LABA/Storage.cs
+++ OOP_LABA/Storage.cs
@@ -8,17 +8,17 @@ namespace OOP_LABA
     {
         const int dop = 10; //дополнительная длина массива
 
-        private T[] arr;
+        private T[] array;
         private int size;
         private int capacity;
         public Storage()
         {
-            arr = null;
+            array = null;
         }
         public Storage(int _size)
         {
             //конструктор с заданной длиной массива
-            arr = new T[_size + dop];
+            array = new T[_size + dop];
             size = _size;
             capacity = _size + dop;
         }
@@ -26,68 +26,68 @@ namespace OOP_LABA
         public void reserve(int _size)
         {
             //задать размер
-            arr = new T[_size + dop];
+            array = new T[_size + dop];
             size = 0;
             capacity = _size + dop;
         }
         public void push_back(T elem)
         {
-            if (arr == null)
+            if (array == null)
             {
-                arr = new T[3];
-                arr[0] = elem;
+                array = new T[3];
+                array[0] = elem;
                 size = 1;
                 capacity = 3;
             }
-            else if (arr != null)
+            else if (array != null)
             {
                 if (size == capacity)
                 {
                     T[] newarr = new T[size + dop];
                     for (int i = 0; i < size; i++)
-                        newarr[i] = arr[i];
+                        newarr[i] = array[i];
                     newarr[size] = elem;
-                    arr = newarr;
+                    array = newarr;
                     capacity = size + dop;
                     size++;
                 }
                 else
                 {
-                    arr[size] = elem;
+                    array[size] = elem;
                     size++;
                 }
             }//вставка в конец
         }
         public void pop_back()
         { //удалить из конца  
-            if (arr != null && size > 0)
+            if (array != null && size > 0)
             {
                 if (size < capacity / 3 && size > 20)
                 {
                     size--;
                     T[] newarr = new T[size + dop];
                     for (int i = 0; i < size; i++)
-                        newarr[i] = arr[i];
-                    arr = newarr;
+                        newarr[i] = array[i];
+                    array = newarr;
                     capacity = size + dop;
                 }
                 else size--;
             }
-            else if (arr == null || size <= 0)
+            else if (array == null || size <= 0)
                 Console.WriteLine("Массив пустой");
         }//удалить последний элемент
         public void insert(T elem, int index)
         {
-            if (arr == null)
+            if (array == null)
             {
-                arr = new T[3];
-                arr[0] = elem;
+                array = new T[3];
+                array[0] = elem;
                 size = 1;
                 capacity = 3;
             }
             else if (size == 0)
             {
-                arr[0] = elem;
+                array[0] = elem;
                 size++;
             }
             else
@@ -110,16 +110,16 @@ namespace OOP_LABA
                             newarr[k] = elem;
                             k++;
                         }
-                        else newarr[k] = arr[i];
+                        else newarr[k] = array[i];
                     }
-                    arr = newarr;
+                    array = newarr;
                     capacity = size + dop;
                 }
                 else
                 {
                     for (int i = size - 1; i >= index; i--)
-                        arr[i + 1] = arr[i];
-                    arr[index] = elem;
+                        array[i + 1] = array[i];
+                    array[index] = elem;
                     size++;
                 }
             }
@@ -128,10 +128,10 @@ namespace OOP_LABA
         public void push_front(T elem)
         {
             //insert(elem, 0);
-            if (arr == null)
+            if (array == null)
             {
-                arr = new T[3];
-                arr[0] = elem;
+                array = new T[3];
+                array[0] = elem;
                 size = 1;
                 capacity = 3;
             }
@@ -143,36 +143,36 @@ namespace OOP_LABA
                     T[] newarr = new T[size + dop];
                     newarr[0] = elem;
                     for (int i = 0; i < size - 1; i++)
-                        newarr[i + 1] = arr[i];
-                    arr = newarr;
+                        newarr[i + 1] = array[i];
+                    array = newarr;
                     capacity = size + dop;
                 }
                 else
                 {
                     for (int i = size - 1; i >= 0; i--)
-                        arr[i + 1] = arr[i];
+                        array[i + 1] = array[i];
                     size++;
-                    arr[0] = elem;
+                    array[0] = elem;
                 }
             }
         }//вставка в начало
         public void pop_front()
         {   //удаляет первый элемент
-            if (arr != null && size > 0)
+            if (array != null && size > 0)
             {
                 if (size < (capacity / 4) && size > 50)
                 {
                     size--;
                     T[] newarr = new T[size + dop];
                     for (int i = 1; i < size + 1; i++)
-                        newarr[i - 1] = arr[i];
-                    arr = newarr;
+                        newarr[i - 1] = array[i];
+                    array = newarr;
                     capacity = size + dop;
                 }
                 else
                 {
                     for (int i = 1; i < size; i++)
-                        arr[i - 1] = arr[i];
+                        array[i - 1] = array[i];
                     size--;
                 }
             }
@@ -180,7 +180,7 @@ namespace OOP_LABA
         }                       //удалить первый элемент
         public void pop(int index)
         { //удалить по индексу 
-            if (arr != null && size > 0)
+            if (array != null && size > 0)
             {
                 if (size < (capacity / 4) && size > 50)
                 {
@@ -189,16 +189,16 @@ namespace OOP_LABA
                     for (int i = 0, k = 0; i < size + 1; i++)
                         if (i != index)
                         {
-                            newarr[k] = arr[i];
+                            newarr[k] = array[i];
                             k++;
                         }
-                    arr = newarr;
+                    array = newarr;
                     capacity = size + dop;
                 }
                 else
                 {
                     for (int i = index + 1; i < size; i++)
-                        arr[i - 1] = arr[i];
+                        array[i - 1] = array[i];
                     size--;
                 }
             }
@@ -208,20 +208,20 @@ namespace OOP_LABA
         {
             get
             {
-                if (i < size && i >= 0 && arr != null)
-                    return arr[i];
+                if (i < size && i >= 0 && array != null)
+                    return array[i];
                 else throw new IndexOutOfRangeException();
             }
             set
             {
-                if (i < size && i >= 0 && arr != null)
-                    arr[i] = value;
+                if (i < size && i >= 0 && array != null)
+                    array[i] = value;
                 else throw new IndexOutOfRangeException();
             }
         }
         public T getanddelete(int index)
         {
-            if (arr != null && size > 0)
+            if (array != null && size > 0)
             {
                 if (size < (capacity / 4) && size > 50)
                 {
@@ -230,19 +230,19 @@ namespace OOP_LABA
                     for (int i = 0, k = 0; i < size + 1; i++)
                         if (i != index)
                         {
-                            newarr[k] = arr[i];
+                            newarr[k] = array[i];
                             k++;
                         }
-                    T r_elem = arr[index];
-                    arr = newarr;
+                    T r_elem = array[index];
+                    array = newarr;
                     capacity = size + dop;
                     return r_elem;
                 }
                 else
                 {
-                    T r_elem = arr[index];
+                    T r_elem = array[index];
                     for (int i = index + 1; i < size; i++)
-                        arr[i - 1] = arr[i];
+                        array[i - 1] = array[i];
                     size--;
                     return r_elem;
                 }
@@ -251,9 +251,9 @@ namespace OOP_LABA
         }
         public T get(int index)
         { //достать элемент 
-            if (arr != null && size > 0)
+            if (array != null && size > 0)
             {
-                T r_elem = arr[index];
+                T r_elem = array[index];
                 return r_elem;
             }
             else throw new Exception("Массив пустой");
@@ -261,7 +261,7 @@ namespace OOP_LABA
         public void clear()
         { //очистка
 
-            arr = null;
+            array = null;
             size = 0;
             capacity = 0;
         }                           //очистка
@@ -275,13 +275,13 @@ namespace OOP_LABA
         }                         //вернуть размер расширенного массива
         public bool empty()
         {  //проверить пустой ли 
-            if (arr == null || size == 0)
+            if (array == null || size == 0)
                 return true;
             else return false;
         }                           //проверка, пустой ли массив
         public void resize(int n)
         { //изменить размер 
-            if (arr == null)
+            if (array == null)
                 reserve(n);
             else if (n > size && n <= capacity)
                 size = n;
@@ -289,9 +289,9 @@ namespace OOP_LABA
             {
                 T[] newarr = new T[n + dop];
                 for (int i = 0; i < size; i++)
-                    newarr[i] = arr[i];
+                    newarr[i] = array[i];
 
-                arr = newarr;
+                array = newarr;
                 size = n;
                 capacity = size + dop;
             }
@@ -300,9 +300,9 @@ namespace OOP_LABA
                 size = n;
                 T[] newarr = new T[size + dop];
                 for (int i = 0; i < size; i++)
-                    newarr[i] = arr[i];
+                    newarr[i] = array[i];
 
-                arr = newarr;
+                array = newarr;
                 capacity = size + dop;
             }
             else if (n < size)
@@ -310,10 +310,10 @@ namespace OOP_LABA
         }                     //задать новый размер массива
         public int Find(T elem)
         { //найти по индексу
-            if (arr != null && size != 0)
+            if (array != null && size != 0)
                 for (int i = 0; i < size; i++)
                 {
-                    if (arr[i].Equals(elem))
+                    if (array[i].Equals(elem))
                         return i;
                 }
             return -1;
